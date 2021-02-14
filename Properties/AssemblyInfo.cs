d8d817109f29f77e4b2c3fa46b833c7ff18e3a8f
rublon-psz <psz@rublon.com>
2021-02-14 07:17:30 +0100

    Changes due to package publication on Nuget

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rublon SDK")]
+[assembly: AssemblyTitle("Rublon")]
 [assembly: AssemblyDescription("Rublon SDK for .Net")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ADIPS Sp. z o.o.")]
