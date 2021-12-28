1ff9c9b21425807e526d4a603c8ab1290076f899
Przemysław Szeremeta <48554629+rublon-psz@users.noreply.github.com>
2021-12-28 08:39:53 +0100

    Rublon interfaces and consumer params changed to params
    * added interfaces, which can be used as mocks in the tests.
    * Consumer Params changed to params. Extended the params builder (former consumer params builder).

---
+++ TwoFactor/IRublon.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using Rublon.Sdk.Core;
+using Rublon.Sdk.Core.Logging;
+
+namespace Rublon.Sdk.TwoFactor
+{
+    public interface IRublon : IRublonConsumer
+    {
+        IRublonLogger Logger { get; set; }
+
+        string Auth(string callbackUrl, string userId, string userEmail);
+        string Auth(string callbackUrl, string userId, string userEmail, JObject consumerParams);
+        Credentials GetCredentials(string accessToken);
+    }
+}
